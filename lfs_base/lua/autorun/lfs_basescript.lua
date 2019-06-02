ca88e5169e495b948588dd7ed65c08bcb6096433
Blu <swagnemite@gmx.de>
2019-06-02 14:56:39 +0200

    fix version number

--- lfs_base/lua/autorun/lfs_basescript.lua
+++ lfs_base/lua/autorun/lfs_basescript.lua
@@ -6,7 +6,7 @@ local meta = FindMetaTable( "Player" )
 simfphys = istable( simfphys ) and simfphys or {} -- lets check if the simfphys table exists. if not, create it!
 simfphys.LFS = {} -- lets add another table for this project. We will be storing all our global functions and variables here. LFS means LunasFlightSchool
 
-simfphys.LFS.VERSION = 136 -- note to self: Workshop is 10-version increments ahead. (next workshop update at 146)
+simfphys.LFS.VERSION = 136 -- note to self: Workshop is 10-version increments ahead. (next workshop update at 147)
 
 simfphys.LFS.KEYS_IN = {}
 simfphys.LFS.KEYS_DEFAULT = {}
